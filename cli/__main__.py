574befeb54e8249bc87f720aeb30d216b33f66b1
Andre Zugman <andre.zugman@gmail.com>
2022-09-27 13:45:50 -0400

    updated error message

--- cli/__main__.py
+++ cli/__main__.py
@@ -21,7 +21,7 @@ from downloadtools.utils import anonymize
 from downloadtools.utils import convert2nii
 #%%
 def main():
-    with xnat.connect("https://fmrif-xnat.nimh.nih.gov") as xsession :
+    
        
         if hasattr(sys, "ps1"):
             project = "01-M-0192"
@@ -63,26 +63,27 @@ def main():
             unzip = True
             downloaddir = args.output
         if not os.path.exists(os.path.join("/home",os.environ["USER"],".netrc")) :
-            print("please configure .netrc file")
-        if dosnapshot :
-         
-            dbsnapshot = checkdatabase(xsession, project)
-            dbsnapshot.to_csv(os.path.join(downloaddir,"dbsnapshot.csv"))
-            download = False   
-        if sdanid :
-            for i in sdanid :
-        
-                print (i)
-                dbsearched = dbreader(i)
-                MRN = dbsearched.loc[0,1]
-                MRN = MRN.replace("-","")
-        
-        if download : 
-            download_dcm(xsession, project, MRN, i, date, SeriesName, downloaddir, unzip )
-            if keepdicom :
-                anonymize(downloaddir, i)
-            else :
-                convert2nii(downloaddir)
+            sys.exit("please configure .netrc file")
+        with xnat.connect("https://fmrif-xnat.nimh.nih.gov") as xsession :
+            if dosnapshot :
+             
+                dbsnapshot = checkdatabase(xsession, project)
+                dbsnapshot.to_csv(os.path.join(downloaddir,"dbsnapshot.csv"))
+                download = False   
+            if sdanid :
+                for i in sdanid :
+            
+                    print (i)
+                    dbsearched = dbreader(i)
+                    MRN = dbsearched.loc[0,1]
+                    MRN = MRN.replace("-","")
+            
+            if download : 
+                download_dcm(xsession, project, MRN, i, date, SeriesName, downloaddir, unzip )
+                if keepdicom :
+                    anonymize(downloaddir, i)
+                else :
+                    convert2nii(downloaddir)
         #%% Just some testing.
         
 
