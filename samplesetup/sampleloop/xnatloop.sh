6a14b2573f73eb2c91399413527c881d9e69dabb
Andre Zugman <andre.zugman@gmail.com>
2022-09-30 13:00:32 -0400

    v=0.1.4

---
+++ samplesetup/sampleloop/xnatloop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Simple bash loop. first argument is a csv file, second is output directory
+#since flags are not positional, you can add them to the lines in your list (see example)
+#example call:
+# xnatloop.sh sample.csv /path/to/download 
+SUBJECTS=${1}
+OUTPUT=${2}
+for i in $( cat ${1} ) ; do
+	xnat-tools -i ${1} -o ${2}
+done
+	
+
